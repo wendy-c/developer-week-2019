664b0f3c4b73bf96b9ccf0323ef3dfcc076886a1
ClarabelleCheng-Yue <cachengyue@gmail.com>
2019-02-23 11:42:16 -0800

    add server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const axios = require('axios');
+
+require('dotenv').config();
+
+const app = express();
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.enable('trust proxy')
+
+app.get('/getdata', async (req, res) => {
+});
+
+app.get('/ping', function(req, res) {
+  return res.send('pong')
+})
+
+app.get('/', function (_req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080, console.log("Listening on port 8080"));
+
