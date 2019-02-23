1509c7e680e3f4aa192eaba91735f9ad20a78472
ClarabelleCheng-Yue <cachengyue@gmail.com>
2019-02-23 12:59:48 -0800

    add env file

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const axios = require('axios');
 
 require('dotenv').config();
 
+console.log('keys: ', process.env);
+
 const app = express();
 app.use(cors())
 app.use(express.static(path.join(__dirname, 'build')));
@@ -23,4 +25,3 @@ app.get('/', function (_req, res) {
 });
 
 app.listen(process.env.PORT || 8080, console.log("Listening on port 8080"));
-
